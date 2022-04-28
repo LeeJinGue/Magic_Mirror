0fadb72cffb725f6a8473799aca90d9510fcf3f2
LeeJinGue <33407139+LeeJinGue@users.noreply.github.com>
2022-04-28 19:10:23 +0900

    1. Message Fragment수정

--- SmartMirror/app/src/main/java/com/cookandroid/smartmirror/navigationFragment/MessageSendFragment.java
+++ SmartMirror/app/src/main/java/com/cookandroid/smartmirror/navigationFragment/MessageSendFragment.java
@@ -10,6 +10,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -22,9 +23,15 @@ import androidx.fragment.app.Fragment;
 import com.cookandroid.smartmirror.R;
 import com.cookandroid.smartmirror.activities.MainScreenActivity;
 
+import java.util.ArrayList;
+
 public class MessageSendFragment extends Fragment {
     MainScreenActivity activity;
-
+    // 프로필 정보 객체를 담아둔 ArrayList
+    ArrayList<Profile> profileList = new ArrayList<>();
+    Button testBtnRegister;
+    // 현재 선택되어있는 프로필의 개수
+    int selectedCount = 0;
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
@@ -36,7 +43,7 @@ public class MessageSendFragment extends Fragment {
         super.onDetach();
         activity = null;
     }
-
+    boolean selected = false;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_message_send, container, false);
@@ -49,10 +56,11 @@ public class MessageSendFragment extends Fragment {
         String profileName = "프로필1";
         boolean test1 = false;
         Profile test = new Profile("테스트", "프로필 "+1, false);
-
+        testBtnRegister = rootView.findViewById(R.id.testBtnRegister);
         for(int i=0; i<2; i++) {
 
             Profile pData = new Profile("테스트", "프로필 "+i, false);
+            profileList.add(pData);
             // 프로필 사진+프로필명 생성을 위한 LinLayout
             LinearLayout profile1 = new LinearLayout(rootView.getContext());
             LinearLayout.LayoutParams profileLinParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
@@ -60,6 +68,7 @@ public class MessageSendFragment extends Fragment {
             profile1.setGravity(Gravity.CENTER);
             profile1.setOrientation(LinearLayout.VERTICAL);
             // 프로필 사진(ImageView)
+            int nowNum = i;
             ImageView profileImg = new ImageView(rootView.getContext());
             profileImg.setImageResource(R.drawable.ic_baseline_image_24);
             LinearLayout.LayoutParams imgViewParams = new LinearLayout.LayoutParams(imgViewWidth, imgViewHeight);
@@ -69,18 +78,22 @@ public class MessageSendFragment extends Fragment {
                 public void onClick(View v) {
                     // 해당 프로필이 선택되었음을 알려줘야 한다.
                     // 선택되어 있을 때
-                    if(test.isSelected){
-                        Log.i("test", pData.name+"의 isSelected: "+pData.isSelected);
+                    Profile nowProfile = profileList.get(nowNum);
+                    if(nowProfile.isSelected){
+                        // 선택되어있는 프로필을 클릭하면 선택이 풀린다.
+//                        Log.i("test", "nowNum: "+nowNum);
                         profile1.setBackgroundColor(getResources().getColor(R.color.white));
-
-//                        pData.isSelected = true;
-                        pData.setSelected(true);
-
+                        nowProfile.isSelected = false;
+                        selectedCount--;
                     }else{
-                        Log.i("test", pData.name+"의 isSelected: "+pData.isSelected);
+                        // 선택이 안 되어있는 프로필을 클릭하면 클릭한 프로필이 선택된다.
+  //                      Log.i("test", "selected: "+selected);
                         profile1.setBackgroundColor(getResources().getColor(R.color.bgColor));
-                        pData.isSelected = false;
+                        nowProfile.isSelected = true;
+                        selectedCount++;
                     }
+                    Log.i("test", "selectedCount: "+selectedCount);
+
 
                 }
             });
