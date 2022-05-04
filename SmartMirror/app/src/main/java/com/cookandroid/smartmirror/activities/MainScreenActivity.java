afd1c6887935ac3a01e908bf2de7d5600cd13d9e
LeeJinGue <33407139+LeeJinGue@users.noreply.github.com>
2022-05-04 20:25:41 +0900

    1. 메인화면 NavigationBar에 소지품확인 추가, 소지품확인 Fragment 추가

--- SmartMirror/app/src/main/java/com/cookandroid/smartmirror/activities/MainScreenActivity.java
+++ SmartMirror/app/src/main/java/com/cookandroid/smartmirror/activities/MainScreenActivity.java
@@ -14,6 +14,7 @@ import androidx.appcompat.widget.Toolbar;
 import com.cookandroid.smartmirror.Methods;
 import com.cookandroid.smartmirror.R;
 import com.cookandroid.smartmirror.dataClass.MyApplication;
+import com.cookandroid.smartmirror.navigationFragment.CheckBelongingsFragment;
 import com.cookandroid.smartmirror.navigationFragment.MessageSendFragment;
 import com.cookandroid.smartmirror.navigationFragment.ScheduleFragment;
 import com.cookandroid.smartmirror.navigationFragment.StockSetupFragment;
@@ -26,6 +27,7 @@ public class MainScreenActivity extends AppCompatActivity {
     MessageSendFragment messageSendFragment_frag;
     ScheduleFragment scheduleFragment_frag;
     StockSetupFragment stockSetupFragment_frag;
+    CheckBelongingsFragment checkBelongingsFragment_frag;
     Toolbar toolbar;
     TextView tvTitle;
 
@@ -54,6 +56,7 @@ public class MainScreenActivity extends AppCompatActivity {
         messageSendFragment_frag = new MessageSendFragment();
         scheduleFragment_frag = new ScheduleFragment();
         stockSetupFragment_frag = new StockSetupFragment();
+        checkBelongingsFragment_frag = new CheckBelongingsFragment();
 
         getSupportFragmentManager().beginTransaction().replace(R.id.container, windowSetupFragment_frag).commit();
 
@@ -84,6 +87,11 @@ public class MainScreenActivity extends AppCompatActivity {
                             case R.id.tab4:
                                 Toast.makeText(getApplicationContext(), "관심주설정", Toast.LENGTH_LONG).show();
                                 getSupportFragmentManager().beginTransaction().replace(R.id.container, stockSetupFragment_frag).commit();
+
+                                return true;
+                            case R.id.tab5:
+                                Toast.makeText(getApplicationContext(), "소지품확인", Toast.LENGTH_LONG).show();
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, checkBelongingsFragment_frag).commit();
 
                                 return true;
                         }
---
+++ SmartMirror/app/src/main/java/com/cookandroid/smartmirror/navigationFragment/CheckBelongingsFragment.java
@@ -0,0 +1,23 @@
+package com.cookandroid.smartmirror.navigationFragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cookandroid.smartmirror.R;
+
+
+public class CheckBelongingsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_check_belongings, container, false);
+    }
+}
