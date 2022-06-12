b6317d75a2a17d4d6494008b04ddcfe029f83630
LeeJinGue <33407139+LeeJinGue@users.noreply.github.com>
2022-06-12 21:08:50 +0900

    Merge중2

---
+++ SmartMirror/app/build/generated/source/buildConfig/release/com/cookandroid/smartmirror/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cookandroid.smartmirror;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.cookandroid.smartmirror";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
